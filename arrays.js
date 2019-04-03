7f83bbde19fa589b43c4e5933b4ecae5ecbde9a6
dennis wang <dwang0816@gmail.com>
2019-04-03 18:31:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -46,27 +46,3 @@ function RemoveElementFromEndOfArray(array) {
 
 
 
-
-
-
-
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  // array = [element, ...array];
-}
-
-function addElementToEndOfArray(array, element){
-  return [...array, element];
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-}
-
-function accessElementInArray(array, index){
-  return array[index];
-}
