ee612c05d20864632c8c758c9fb1b9cadca101e6
dennis wang <dwang0816@gmail.com>
2019-04-03 17:30:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,33 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+
+
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  // array = [element, ...array];
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
